ee4d16f7077572eed17455079442e6bc762539f0
Pk-hh <pradeep4045t7@gmail.com>
2025-05-21 12:57:35 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,31 @@
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open('planner-cache').then(cache => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/manifest.json',
-        'https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap',
-        'https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js'
-      ]);
-    })
+const CACHE_NAME = 'daily-planner-cache-v1';
+const FILES_TO_CACHE = [
+  '.',
+  'index.html',
+  'manifest.json',
+  'service-worker.js',
+  'icon-192.png',
+  'icon-512.png'
+];
+
+self.addEventListener('install', (evt) => {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
   self.skipWaiting();
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+self.addEventListener('activate', (evt) => {
+  evt.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', evt => {
+  evt.respondWith(
+    caches.match(evt.request).then(resp => resp || fetch(evt.request))
   );
 });
